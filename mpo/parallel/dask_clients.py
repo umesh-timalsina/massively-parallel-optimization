e10e993f1ec9da7a29e875689728a6f120f43228
Umesh Timalsina <timalsu@gw344.vampire>
2023-02-13 11:55:28 -0600

    Add ACCRE config

--- mpo/parallel/dask_clients.py
+++ mpo/parallel/dask_clients.py
@@ -1,5 +1,7 @@
 from dask.distributed import Client, LocalCluster
 
+from dask_jobqueue import SLURMCluster
+
 from mpo.utils import RegistersMPOConstruct
 
 
@@ -12,6 +14,30 @@ def local_cluster_client(
     cluster = LocalCluster(
         name="localCluster", n_workers=num_workers, processes=True
     )
+
+    client = Client(cluster)
+
+    return client
+
+
+@RegistersMPOConstruct(construct_name="runners", construct_key="ACCREClient")
+def accre_client(
+    num_workers=2,
+    jobs=2,
+    queue="debug",
+    memory="8GB",
+    cores=8,
+    interface="em1"
+):
+    cluster = SLURMCluster(
+        n_workers=num_workers,
+        queue=queue,
+        memory=memory,
+        cores=cores,
+        interface=interface 
+    )   
+
+    cluster.scale(jobs=jobs)
 
     client = Client(cluster)
 
