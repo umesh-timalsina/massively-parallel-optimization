47758d1a95893366189137a1e44408de6c9e7d8e
Umesh Timalsina <timalsu@gw344.vampire>
2023-02-13 12:05:59 -0600

    Fix linting issues

--- mpo/parallel/dask_clients.py
+++ mpo/parallel/dask_clients.py
@@ -1,5 +1,4 @@
 from dask.distributed import Client, LocalCluster
-
 from dask_jobqueue import SLURMCluster
 
 from mpo.utils import RegistersMPOConstruct
@@ -22,20 +21,15 @@ def local_cluster_client(
 
 @RegistersMPOConstruct(construct_name="runners", construct_key="ACCREClient")
 def accre_client(
-    num_workers=2,
-    jobs=2,
-    queue="debug",
-    memory="8GB",
-    cores=8,
-    interface="em1"
+    num_workers=2, jobs=2, queue="debug", memory="8GB", cores=8, interface="em1"
 ):
     cluster = SLURMCluster(
         n_workers=num_workers,
         queue=queue,
         memory=memory,
         cores=cores,
-        interface=interface 
-    )   
+        interface=interface,
+    )
 
     cluster.scale(jobs=jobs)
 
